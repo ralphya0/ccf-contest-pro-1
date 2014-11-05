36ba242f7d683fa38f7cca71907b4aaeef7366e3
ralphya0 <ralphya0@foxmail.com>
2014-11-05 21:00:56 +0800

    对all_important字段进行排序
    Signed-off-by: ralphya0 <ralphya0@foxmail.com>

--- src/me/ralphya0/pre_processing/AggregateByDate.java
+++ src/me/ralphya0/pre_processing/AggregateByDate.java
@@ -13,6 +13,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,9 +39,9 @@ public class AggregateByDate {
         String in1 = "F:\\work-space\\project-base\\ccf\\data\\公共安全事件\\result\\2014-11-4\\violence.txt";
         String in2 = "F:\\work-space\\project-base\\ccf\\data\\公共安全事件\\result\\2014-11-4\\campus.txt";
         String in3 = "F:\\work-space\\project-base\\ccf\\data\\公共安全事件\\result\\2014-11-4\\bus_explosion.txt";
-        String sql1 = "select idnum,person,location,time_tag,first_time,cosin from violence where release_date_day = '#' and cosin > 0.05";
-        String sql2 = "select idnum,person,location,time_tag,first_time,cosin from campus where release_date_day = '#' and cosin > 0.05";
-        String sql3 = "select idnum,person,location,time_tag,first_time,cosin from bus_explosion where release_date_day = '#' and cosin > 0.05";
+        String sql1 = "select idnum,person,location,time_tag,first_time,cosin,all_important from violence where release_date_day = '#' and cosin > 0.05";
+        String sql2 = "select idnum,person,location,time_tag,first_time,cosin,all_important from campus where release_date_day = '#' and cosin > 0.05";
+        String sql3 = "select idnum,person,location,time_tag,first_time,cosin,all_important from bus_explosion where release_date_day = '#' and cosin > 0.05";
         
         String sql4 = "select * from violence where idnum = #";
         String sql5 = "select * from campus where idnum = #";
@@ -48,12 +50,16 @@ public class AggregateByDate {
         String update1 = "insert into yx_daytopic_violence values(";
         String update2 = "insert into yx_daytopic_campus values(" ;
         String update3 = "insert into yx_daytopic_bus_explosion values(";
+        
+        String update4 = "insert into yx_daytopic_violence_new values(";
+        String update5 = "insert into yx_daytopic_campus_new values(" ;
+        String update6 = "insert into yx_daytopic_bus_explosion_new values(";
         init(in1);
-        filter(sql1,sql4,update1);
+        filter(sql1,sql4,update1,update4);
         init(in2);
-        filter(sql2,sql5,update2);
+        filter(sql2,sql5,update2,update5);
         init(in3);
-        filter(sql3,sql6,update3);
+        filter(sql3,sql6,update3,update6);
         st.close();
         connection.close();
         System.out.println("done");
@@ -83,19 +89,28 @@ public class AggregateByDate {
     }
     
     //数据表中日期字段是？需要写入的相关值字段叫啥？新表名？
-    public void filter(String query,String sql2,String update) throws SQLException{
-        
+    public void filter(String query,String sql2,String update,String update2) throws SQLException{
+        Map<String,Integer> peopleCache = new HashMap<String,Integer>();
+        Map<String,Integer> locationCache = new HashMap<String,Integer>();
+        Map<String,Integer> timeCache = new HashMap<String,Integer>();
+        Map<String,Integer> tagCache = new HashMap<String,Integer>();
+        Map<String,Map<String,Double>> imp_cache = new HashMap<String,Map<String,Double>>();
         ResultSet rs = null;
         int ct = 0;
         for(String date : dates){
+            peopleCache.clear();
+            locationCache.clear();
+            timeCache.clear();
+            tagCache.clear();
             rs = st.executeQuery(query.replace("#", date));
-            Map<String,Integer> peopleCache = new HashMap<String,Integer>();
-            Map<String,Integer> locationCache = new HashMap<String,Integer>();
-            Map<String,Integer> timeCache = new HashMap<String,Integer>();
-            Map<String,Integer> tagCache = new HashMap<String,Integer>();
+            imp_cache.clear();
             int counter = 0;
             int max_cosin_idnum = -1;
             double max_cosin = -1;
+            
+            
+            
+            
             //分别统计人物、地点、时间戳到出现次数，为后续处理做准备
             while(rs.next()){
                 counter ++;
@@ -105,6 +120,7 @@ public class AggregateByDate {
                 String time = rs.getString("first_time");
                 String time_tag = rs.getString("time_tag");
                 double cosin = rs.getDouble("cosin");
+                String all_impor = rs.getString("all_important");
                 
                 //分割字段
                 if(people != null && people.trim().length() > 0){
@@ -150,12 +166,41 @@ public class AggregateByDate {
                 
                 if(time_tag != null && time_tag.trim().length() > 0){
                     String [] arr_tag = time_tag.split(" ");
-                    for(String t : arr_tag){
-                        if(t != null && t.trim().length() > 0)
-                            if(!tagCache.containsKey(t))
-                                tagCache.put(t, 1);
-                            else
-                                tagCache.put(t, tagCache.get(t) + 1);
+                    if(arr_tag != null)
+                        for(String t : arr_tag){
+                            if(t != null && t.trim().length() > 0)
+                                if(!tagCache.containsKey(t))
+                                    tagCache.put(t, 1);
+                                else
+                                    tagCache.put(t, tagCache.get(t) + 1);
+                        }
+                }
+                
+                //以计算平均值的方式生成all_important字段
+                if(all_impor != null){
+                    String [] arr_imp = all_impor.split("#");
+                    if(arr_imp != null){
+                        for(String i : arr_imp){
+                            if(i != null && i.trim().length() > 0){
+                                String [] tmp = i.split("/");
+                                if(tmp != null && !tmp[1].equals("ns")&& !tmp[1].equals("nsf") && !tmp[1].equals("nr")
+                                        && !tmp[1].equals("nr1") && !tmp[1].equals("nr2") && !tmp[1].equals("nrj") 
+                                        && !tmp[1].equals("nrf")){
+                                    String wd = tmp[0] + "/" + tmp[1];
+                                    double val = Double.parseDouble(tmp[2]);
+                                    if(!imp_cache.containsKey(wd)){
+                                        Map<String,Double> mm = new HashMap<String,Double>();
+                                        mm.put("sum", val);
+                                        mm.put("count", (double) 1);
+                                        imp_cache.put(wd, mm);
+                                    }
+                                    else{
+                                        imp_cache.get(wd).put("sum", imp_cache.get(wd).get("sum") + val);
+                                        imp_cache.get(wd).put("count", imp_cache.get(wd).get("count") + 1);
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
                 
@@ -166,10 +211,40 @@ public class AggregateByDate {
 
             }
             
+            StringBuilder sb = new StringBuilder();
+            //计算新all_important
+            String [] arr_imp = imp_cache.keySet().toArray(new String[0]);
+            if(arr_imp != null){
+                AItem [] arr = new AItem[arr_imp.length];
+                int cct = 0;
+                for(String s : arr_imp){
+                    if(s != null && s.trim().length() > 0){
+                        double res = imp_cache.get(s).get("sum") / imp_cache.get(s).get("count");
+                        arr[cct] = new AItem(s,res);
+                        cct ++;
+                    }
+                }
+                
+                Arrays.sort(arr, new AIComparator());
+                int ct2 = 1;
+                for(AItem ai : arr){
+                    if(ct2 <= 20){
+                        sb.append(ai.wd + "/" + ai.val + "#");
+                        ct2 ++;
+                    }else
+                        break;
+                }
+                if(sb.lastIndexOf("#") >= 0 && sb.lastIndexOf("#") == sb.length() - 1)
+                    sb.deleteCharAt(sb.length() - 1);
+            }
+                
+                
+            
+            
             //对缓存内容排序并截取结果的前x条
             String people_field = sort(peopleCache,counter / 12,3," ");
             String location_field = sort(locationCache,counter / 12,3,"#");
-            String time_field = sort(timeCache,counter / 10,1," ");
+            String time_field = sort(timeCache,counter / 8,1," ");
             String tag_field = sort(tagCache,counter / 15,1," ");
             double val_field = relative_values.get(ct);
             ct ++;
@@ -212,8 +287,20 @@ public class AggregateByDate {
                         + order_time + "','" + time + "','" + content_media_name + "'," + words
                         + "," + validate_tag + "," + max_cosin + "," + score + "," + analogy
                         + "," + counter + "," + val_field + ")";
+                String p = update2 + max_cosin_idnum + ",'" + id + "'," + url_crc
+                        + ",'" + release_date + "'," + source_type + ",'" + media_name + "','"
+                        + release_date_day + "','" + title + "','" + format_content
+                        + "'," + siteurl_crc + ",'" + hit_tag + "','" + abs + "','" + type
+                        + "','" + title_important + "','" + abs_important + "','" + content_important
+                        + "','" + sb.toString() + "','" + people_field + "','" + location_field
+                        + "','" + release_time + "','" + tag_field + "','" + time_field + "','"
+                        + order_time + "','" + time + "','" + content_media_name + "'," + words
+                        + "," + validate_tag + "," + max_cosin + "," + score + "," + analogy
+                        + "," + counter + "," + val_field + ")";
                 System.out.println(q);
+                
                 st.executeUpdate(q);
+                st.executeUpdate(p);
                 rs2.close();
             }
             
@@ -264,4 +351,31 @@ public class AggregateByDate {
     
 }
 
+class AItem{
+    String wd;
+    double val;
+    public AItem(String i,double c){
+        wd = i;
+        val = c;
+    }
+}
+class AIComparator implements Comparator<AItem>{
+
+    @Override
+    public int compare(AItem o1, AItem o2) {
+        if(o1 != null && o2 != null){
+            
+            double vala = o1.val;
+            double valb = o2.val;
+            if(vala < valb)
+                return 1;
+            if(vala > valb)
+                return -1;
+            return 0;
+        }
+        return 0;
+        
+    }
+
+}
 
